2f600c0cc689b2cf5c4a33313552832e37d58bc5
kaidranzerr <ashstark68@gmail.com>
2025-11-04 17:16:02 +0530

    lecture 1 done!!

---
+++ AI_AGENTS/Agentic1.py
@@ -0,0 +1,9 @@
+# Langfuse --> evaluation of ai agents and LLMs
+# agents takes you from point A to B in life and adds value || it also has access to tools || it interacts with external environment
+# agent needs to have memory || human in loop for evaluation
+
+# ai agent --> an ai model capable of reasoning , planning and interacting with it's environment
+# an agent has agency --> ability to interact with it's environment
+
+# agency level --> amount of control that agent has || highest multi agent --> one agentic workflow starts another agentic workflow
+# language models are the brain of agents
