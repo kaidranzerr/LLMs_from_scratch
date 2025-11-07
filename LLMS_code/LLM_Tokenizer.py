97a45b1e487e7c1abb5a3a592ae664f593db662d
kaidranzerr <ashstark68@gmail.com>
2025-11-07 13:52:01 +0530

    inverse and decode methods

--- LLMS_code/LLM_Tokenizer.py
+++ LLMS_code/LLM_Tokenizer.py
@@ -14,4 +14,18 @@ preprocessed = re.split(r'([,.:;!&?_"()\']|\s)' , raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 print(preprocessed[:30])
 
-# vocabulary
+# vocabulary --> map these tokens in alphabetical order to unique ids
+# each unique token is mapped to an unique integer called token id
+
+# create token ids
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+print(vocab_size)
+
+vocab = {token:integer for integer,token in enumerate(all_words)}
+for i ,item in enumerate(vocab.items()):
+    print(item)
+    if i >= 50:
+        break
+
+# inverse version of vocab is also necessary that maps token_ids back to original text
