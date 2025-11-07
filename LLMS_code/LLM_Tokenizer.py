89913d159aba981cdd10377b66148ec83e70e8fc
kaidranzerr <ashstark68@gmail.com>
2025-11-07 16:28:25 +0530

    tokenizer V2 done!

--- LLMS_code/LLM_Tokenizer.py
+++ LLMS_code/LLM_Tokenizer.py
@@ -52,3 +52,29 @@ ids = tokenizer.encode(text)
 print(ids)
 
 # special context tokens to deal with words which might not be present in vocab
+# |<end of text>| tokens signals the start or end of a segment
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend({"<|endoftext|>" , "<|unk|>"})
+vocab = {token:integer for integer , token in enumerate(all_tokens)}
+print(len(vocab.items()))
+
+class SimpleTokenizerV2:
+    def __init__(self , vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    def encode(self , text):
+        preprocessed = re.split(r'([,.:;!&?_"()\']|\s)' , text) 
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]       
+        preprocessed = [
+            item if item in self.str_to_int else "|<unk>|" for item in preprocessed
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids 
+    def decode(self , ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # replace spaces before specified punctuations
+        text = re.sub(r'\s+([,.!?"()\])' , 'r\1' , text)
+        return text 
