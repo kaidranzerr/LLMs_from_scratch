b6fa6ecd1e59e999847be853fac56397100e33b7
kaidranzerr <ashstark68@gmail.com>
2025-11-07 14:00:43 +0530

    tokenizerV1 done bitch

--- LLMS_code/LLM_Tokenizer.py
+++ LLMS_code/LLM_Tokenizer.py
@@ -29,3 +29,19 @@ for i ,item in enumerate(vocab.items()):
         break
 
 # inverse version of vocab is also necessary that maps token_ids back to original text
+class SimpleTokenizerV1:
+    def __init__(self , vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    def encode(self , text):
+        preprocessed = re.split(r'([,.:;!&?_"()\']|\s)' , text) 
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]       
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids 
+    def decode(self , ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # replace spaces before specified punctuations
+        text = re.sub(r'\s+([,.!?"()\])' , 'r\1' , text)
+        return text 
