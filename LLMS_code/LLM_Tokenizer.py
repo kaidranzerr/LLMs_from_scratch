ca5bc20a7606355e6b4f1b9765c630af54e66d8a
kaidranzerr <ashstark68@gmail.com>
2025-11-07 13:36:40 +0530

    creating the vocab!

--- LLMS_code/LLM_Tokenizer.py
+++ LLMS_code/LLM_Tokenizer.py
@@ -1,3 +1,17 @@
+import re
+
 with open('the-verdict.txt' , "r" , encoding='utf-8') as f:
     raw_text = f.read()
-    
+
+text = "Hello World. This is a test"
+result = re.split(r'([,.:;!&?_"()\']|\s)' , text)
+print(result)
+
+res = [item for item in result if item.strip()] #getting rid of whitespaces
+print(res)
+
+preprocessed = re.split(r'([,.:;!&?_"()\']|\s)' , raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(preprocessed[:30])
+
+# vocabulary
