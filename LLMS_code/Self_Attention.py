bf0b93c1e8fa162888d83a761bb9e577c00252c5
kaidranzerr <ashstark68@gmail.com>
2025-11-08 22:55:32 +0530

    simplified_attention mechanism!

---
+++ LLMS_code/Self_Attention.py
@@ -0,0 +1,5 @@
+# Self-Attention
+# encoder compress entire input sequence into a single hidden state vector
+# in self attention the self refers to the mechanism ability to compute attention weights by relating different positions in a single
+# input sequence
+# it learns the relationships between various parts of input itself , such as words in sentence
