15223cbb14532a9cabddfa7b312db425685cee77
kaidranzerr <ashstark68@gmail.com>
2025-11-09 12:05:06 +0530

    context vector computed

--- LLMS_code/Self_Attention_2.py
+++ LLMS_code/Self_Attention_2.py
@@ -37,4 +37,21 @@ attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5 , dim=-1)
 
 context_vec_2 = attn_weights_2 @ values 
 
-# Final function implementation
+# Final function implementation 
+import torch.nn as nn 
+class SelfAttention_v1(nn.Module):
+    def __init__(self , d_in , d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in , d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in , d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in , d_out))
+    
+    def forward(self , x):
+        keys = x @ self.W_key 
+        queries = x @ self.W_query
+        values = x @ self.W_value
+
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax( attn_scores / keys.shape[-1] ** 0.5 , dim = -1)
+        context_vec = attn_weights @ values 
+        return context_vec 
