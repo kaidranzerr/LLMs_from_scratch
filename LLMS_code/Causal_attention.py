13bab0107e56a0a19b071b57429b5e4ede3433c2
kaidranzerr <ashstark68@gmail.com>
2025-11-09 14:04:46 +0530

    complete algo implemented!

--- LLMS_code/Causal_attention.py
+++ LLMS_code/Causal_attention.py
@@ -33,3 +33,37 @@ print(attn_weights)
 # masking additional attention weights with dropout
 # dropout is a deep learning technique where randomly selected hidden layer units are ignored during training
 # improves overfitting and improves generalization performance 
+
+# using dropout rate of 50% 
+torch.manual_seed(123)
+dropout = torch.nn.Dropout(0.5)
+example = torch.ones(6,6)
+print(dropout(example))
+print(dropout(attn_weights))
+
+# complete algorithm implementation
+batch = torch.stack((inputs , inputs) , dim = 0)
+print(batch.shape)
+
+class CausalAttention(nn.Module):
+    def __init__(self , d_in , d_out , context_length , dropout , qkv_bias = False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in , d_out , bias=qkv_bias)
+        self.W_key = nn.Linear(d_in , d_out , bias=qkv_bias)
+        self.W_value = nn.Linear(d_in , d_out , bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('mask' , torch.triu(torch.ones(context_length , context_length), diagonal = 1))
+
+    def forward(self , x):
+        b , num_tokens , d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1,2)
+        attn_scores.masked_fill(self.mask.bool()[:num_tokens , :num_tokens] , -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5 , dim=-1)
+        attn_weights = self.dropout(attn_weights) 
+        context_vec = attn_weights @ values
+        return context_vec
