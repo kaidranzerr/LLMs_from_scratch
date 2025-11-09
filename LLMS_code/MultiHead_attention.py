49c0ba6dafaf29e26d026ed822291188aa87b61f
kaidranzerr <ashstark68@gmail.com>
2025-11-09 16:20:35 +0530

    multihead attention implemented

--- LLMS_code/MultiHead_attention.py
+++ LLMS_code/MultiHead_attention.py
@@ -3,4 +3,11 @@
 # 1.Implementing multi-head attention involves creating multiple instances of self-atttention mechanism each with it's own weights and
 # then combining their outputs , this can be computationally intensive but it makes LLMs powerful at complex pattern recognition tasks
 
-# extending single head attention to multi head attention
+# extending single head attention to multi head attention 
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self , d_in , d_out , context_length , dropout , num_heads , qkv_bias=False):
+        super.__init__()
+        self.heads = nn.ModuleList([CausalAttention(d_in , d_out , context_length , dropout , qkv_bias)
+                                    for _ in range(num_heads)])
+    def forward(self,x):
+        return torch.cat([head(x) for head in self.heads] , dim=-1)
