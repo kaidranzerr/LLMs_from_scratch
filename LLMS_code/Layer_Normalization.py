92fbc6f740cca60229f4dd0ea5612dd78feac9d0
kaidranzerr <ashstark68@gmail.com>
2025-11-10 15:04:32 +0530

    Layer Normalization implemented!!

--- LLMS_code/Layer_Normalization.py
+++ LLMS_code/Layer_Normalization.py
@@ -11,3 +11,23 @@ batch_example = torch.randn(2,5)
 layer = nn.Sequential(nn.Linear(5,6) , nn.ReLU())
 out = layer(batch_example)
 print(out)
+
+mean = out.mean(dim=-1 , keepdim=True)
+var = out.var(dim=-1 , keepdim=True)
+
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1 , keepdim=True)
+var = out_norm.var(dim=-1 , keepdim=True)
+
+class LayerNorm(nn.Module):
+    def __init__(self , emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self , x):
+        mean = x.mean(dim=-1 , keepdim=True)
+        var = x.var(dim=-1 , keepdim=True , unbiased=False)
+        norm_x = (x-mean) / torch.sqrt(var+self.eps)
+        return self.scale * norm_x + self.shift 
