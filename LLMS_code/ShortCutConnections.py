908641ba8a533517dcaf2e20e39c80985a469b26
kaidranzerr <ashstark68@gmail.com>
2025-11-11 14:10:11 +0530

    shortcut connections implementation

---
+++ LLMS_code/ShortCutConnections.py
@@ -0,0 +1,5 @@
+# they are also known as skip connection or residual connections || to solve problem of vanishing gradient 
+# gradients become progressively small as they propagate backwards 
+# shortcut connection create an alternative path for the gradient to flow by skipping one or more layers 
+# this is acheived by adding an output of a layer to a latter layer 
+# +1 term keeps the gradient flowing through network
