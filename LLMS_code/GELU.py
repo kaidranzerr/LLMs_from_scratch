e100f3e716503895fadb417ecd030921a541600b
kaidranzerr <ashstark68@gmail.com>
2025-11-11 13:53:26 +0530

    feed forward neural network implemented!

--- LLMS_code/GELU.py
+++ LLMS_code/GELU.py
@@ -17,3 +17,20 @@ class GELU(nn.Module):
         return 0.5 * x * (1 + torch.tanh(
             torch.sqrt(torch.tensor(2.0 / torch.pi)) *(x+0.044715 * torch.pow(x,3))
         ))
+
+class FeedForward(nn.Module):
+    def __init__(self , cfg):
+        super.__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"] , 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"] , cfg["emb_dim"])
+        )
+
+    def forward(self , x):
+        return self.layers(x)
+    
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2,3,768)
+out = ffn(x)
+print(out.shape)
