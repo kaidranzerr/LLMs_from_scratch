7eb0a65985119accfd30f55829f0c69fcab1665a
kaidranzerr <ashstark68@gmail.com>
2025-11-14 14:11:53 +0530

    forward method done!

--- LLMS_code/Transformer_block.py
+++ LLMS_code/Transformer_block.py
@@ -8,7 +8,8 @@
 
 # self attention analyzes relationship between input elements
 # feed forward network --> modifies data individually at each position
-
+import torch
+from torch import nn
 GPT_CONFIG_124M = {
     "vocab_size":50257,
     "context_length":1024,
@@ -18,3 +19,34 @@ GPT_CONFIG_124M = {
     "drop_rate":0.1,
     "qkv_bias":False
 }
+
+class TransformerBlock(nn.Module):
+    def __init__(self , cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in = cfg["emb_dim"],
+            d_out = cfg["emb_dim"],
+            context_length = cfg["context_length"],
+            num_heads = cfg["num_heads"],
+            dropout = cfg["drop_rate"],
+            qkv_bias = cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self , x):
+        # shortcut connection for attention block
+        shortcut = x 
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut 
+
+        # shortcut connection for feed forward block
+        shortcut = x 
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
