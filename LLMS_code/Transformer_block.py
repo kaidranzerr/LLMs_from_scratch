8264da361447fa54f9de2933b4738d2125708d2e
kaidranzerr <ashstark68@gmail.com>
2025-11-14 13:58:20 +0530

    gpt-2 config

--- LLMS_code/Transformer_block.py
+++ LLMS_code/Transformer_block.py
@@ -8,3 +8,13 @@
 
 # self attention analyzes relationship between input elements
 # feed forward network --> modifies data individually at each position
+
+GPT_CONFIG_124M = {
+    "vocab_size":50257,
+    "context_length":1024,
+    "emb_dim":768,
+    "n_heads":12,
+    "n_layers":12,
+    "drop_rate":0.1,
+    "qkv_bias":False
+}
