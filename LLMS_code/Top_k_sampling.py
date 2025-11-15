88601f4abbd76cc7c7027727663fc48f3e934817
kaidranzerr <ashstark68@gmail.com>
2025-11-16 00:15:10 +0530

    implementation done!!

--- LLMS_code/Top_k_sampling.py
+++ LLMS_code/Top_k_sampling.py
@@ -5,3 +5,9 @@ torch.manual_seed(123)
 next_token_id = torch.multinomial(probas , num_samples=1).item()
 
 # we want to restrict the oppurtunity of next token to only a few tokens
+
+top_K = 3
+top_logits , top_pos = torch.topk(next_token_logits , top_k)
+# only the top tokens have the oppurtunity to become the next tokens
+
+# logits --> top-k --> logits / temp --> softmax --> sample from multinomial
